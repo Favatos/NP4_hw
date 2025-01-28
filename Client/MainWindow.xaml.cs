2fdb2cfb347b6024e0fc958e57d6bdb5dcd2ad29
werty werty <werty36401@gmail.com>
2025-01-28 13:37:07 +0300

    Добавьте файлы проекта.

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Net.Sockets;
+using System.Windows;
+using TcpLib;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSelect_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog() == true)
+            {
+                t1.Text = dialog.FileName;
+            }
+        }
+
+        private async void ButtonSend_Click(object sender, RoutedEventArgs e)
+        {
+            if (t1.Text.Length==0)
+            {
+                MessageBox.Show("Please select file first", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            using TcpClient server = new TcpClient("localhost", 2025);
+
+            await server.SendStringAsync(t1.Text);
+            using Stream stream = File.OpenRead(t1.Text);
+            await server.SendFileAsync(stream);
+
+            t1.Text = "";
+            MessageBox.Show("File was sent successfully", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using TcpLib;
+
+namespace Server;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        using TcpListener listener = new TcpListener(IPAddress.Any, 2025);
+        listener.Start();
+        Console.WriteLine($"Server is running on {listener.Server.LocalEndPoint}");
+
+        while (true)
+        {
+            using TcpClient client = await listener.AcceptTcpClientAsync();
+            Console.WriteLine($"The client {client.Client.RemoteEndPoint} has connected to the server");
+
+            string title = Path.GetFileName(await client.ReceiveStringAsync());
+            using Stream stream = File.Create(title);
+            await client.ReceiveFileAsync(stream);
+        }
+    }
+}
